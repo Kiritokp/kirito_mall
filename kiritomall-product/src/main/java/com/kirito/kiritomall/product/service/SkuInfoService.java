2ac8b754fe68fb377f18f984e7939e3aad58a7e9
kirito <1350221894@qq.com>
2021-12-21 23:58:46 +0800

    SKU检索

--- kiritomall-product/src/main/java/com/kirito/kiritomall/product/service/SkuInfoService.java
+++ kiritomall-product/src/main/java/com/kirito/kiritomall/product/service/SkuInfoService.java
@@ -16,5 +16,7 @@ import java.util.Map;
 public interface SkuInfoService extends IService<SkuInfoEntity> {
 
     PageUtils queryPage(Map<String, Object> params);
+
+    PageUtils querySkuInfo(Map<String, Object> params);
 }
 
--- kiritomall-product/src/main/java/com/kirito/kiritomall/product/service/impl/SkuInfoServiceImpl.java
+++ kiritomall-product/src/main/java/com/kirito/kiritomall/product/service/impl/SkuInfoServiceImpl.java
@@ -1,5 +1,7 @@
 package com.kirito.kiritomall.product.service.impl;
 
+import org.apache.commons.lang.StringUtils;
+import org.apache.ibatis.mapping.ParameterMap;
 import org.springframework.stereotype.Service;
 import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -22,6 +24,31 @@ public class SkuInfoServiceImpl extends ServiceImpl<SkuInfoDao, SkuInfoEntity> i
                 new Query<SkuInfoEntity>().getPage(params),
                 new QueryWrapper<SkuInfoEntity>()
         );
+
+        return new PageUtils(page);
+    }
+
+    @Override
+    public PageUtils querySkuInfo(Map<String, Object> params) {
+        String catelogId = (String) params.get("catelogId");
+        String brandId = (String) params.get("brandId");
+        String min = (String) params.get("min");
+        String max = (String) params.get("max");
+        String key = (String) params.get("key");
+
+        QueryWrapper<SkuInfoEntity> wrapper = new QueryWrapper<>();
+        if (!StringUtils.isEmpty(catelogId)){
+            wrapper.eq("catalog_id",catelogId);
+        }
+        if (!StringUtils.isEmpty(brandId)){
+            wrapper.eq("brand_id",brandId);
+        }
+        if (!StringUtils.isEmpty(key)){
+            wrapper.eq("sku_id",key).or().like("sku_name",key);
+        }
+        wrapper.ge("price",min).le("price",max);
+
+        IPage<SkuInfoEntity> page = this.page(new Query<SkuInfoEntity>().getPage(params), wrapper);
 
         return new PageUtils(page);
     }
