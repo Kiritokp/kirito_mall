edffbb1931009ea34e48885603047f6510684ba2
kirito <1350221894@qq.com>
2021-12-23 14:44:48 +0800

    库存模块初始化构建

--- kiritomall-ware/src/main/java/com/kirito/kiritomall/ware/KiritomallWareApplication.java
+++ kiritomall-ware/src/main/java/com/kirito/kiritomall/ware/KiritomallWareApplication.java
@@ -1,8 +1,12 @@
 package com.kirito.kiritomall.ware;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@MapperScan("com.kirito.kiritomall.ware.dao")
+@EnableFeignClients
 @SpringBootApplication
 public class KiritomallWareApplication {
 
