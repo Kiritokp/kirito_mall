27a5945cc8f1da6839158d85d6c2e19d4b65efb4
kirito <1350221894@qq.com>
2021-12-29 10:48:21 +0800

    Elasticsearch检索模块构建

---
+++ kiritomall-search/src/main/java/com/kirito/kiritomall/search/KiritomallSearchApplication.java
@@ -0,0 +1,13 @@
+package com.kirito.kiritomall.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiritomallSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KiritomallSearchApplication.class, args);
+    }
+
+}
---
+++ kiritomall-search/src/test/java/com/kirito/kiritomall/search/KiritomallSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kirito.kiritomall.search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiritomallSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
