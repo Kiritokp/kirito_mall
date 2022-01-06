adc803ccfa8716657c19f2df8fd3ad95cb4e312a
kirito <1350221894@qq.com>
2022-01-06 13:35:28 +0800

    ES模块搭建完善

---
+++ kiritomall-search/src/main/java/com/kirito/kiritomall/search/config/ElasticsearchConfig.java
@@ -0,0 +1,20 @@
+package com.kirito.kiritomall.search.config;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ElasticsearchConfig {
+
+    @Bean
+    public RestHighLevelClient esRestClient(){
+        RestHighLevelClient client = new RestHighLevelClient(
+                RestClient.builder(
+                        new HttpHost("192.168.56.10", 9200, "http")));
+        return client;
+    }
+
+}
