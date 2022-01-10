762c93c7aaf0ed210a8dcddfd11af70d0d51a822
kirito <1350221894@qq.com>
2022-01-10 16:05:47 +0800

    远程调用的两种方式

--- kiritomall-ware/src/main/java/com/kirito/kiritomall/ware/feign/ProductFeignService.java
+++ kiritomall-ware/src/main/java/com/kirito/kiritomall/ware/feign/ProductFeignService.java
@@ -7,7 +7,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @FeignClient("kiritomall-product")
+//@FeignClient("kiritomall-gateway")
 public interface ProductFeignService {
+    /**
+     * 远程调用的两种方式
+     * 1./product/skuinfo/info/{skuId} 直接调用对用的服务
+     * 2./api/product/skuinfo/info/{skuId} 所有请求都要通过网关
+     * @param skuId
+     * @return
+     */
     @GetMapping("product/skuinfo/info/{skuId}")
     public R info(@PathVariable("skuId") Long skuId);
 }
