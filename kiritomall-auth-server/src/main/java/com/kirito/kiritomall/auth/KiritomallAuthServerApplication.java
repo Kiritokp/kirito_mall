b549f39329c93dc2d7c4268a8dd341fa3a2d8875
kirito <1350221894@qq.com>
2022-06-21 15:09:43 +0800

    认证中心（社交登录、OAuth2.0、单点登录）服务搭建

---
+++ kiritomall-auth-server/src/main/java/com/kirito/kiritomall/auth/KiritomallAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.kirito.kiritomall.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiritomallAuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KiritomallAuthServerApplication.class, args);
+    }
+
+}
---
+++ kiritomall-auth-server/src/test/java/com/kirito/kiritomall/auth/KiritomallAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kirito.kiritomall.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiritomallAuthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
